1f8be64c9544b3d2a80badce7945bf76f2debdfe
yanxinle <1020121123@qq.com>
2024-04-27 15:50:51 +0800

    重构代码

--- models/change_image.js
+++ models/change_image.js
@@ -3,3 +3,15 @@ function changeImage(id, changeSrc) {
     //如果src属性中有bulbon这个单词，则将其改为下面这个src
     element.src = changeSrc;
 }
+
+let imageNumber = 1;
+
+function changeImage2(id, changeSrc1, changeSrc2) {
+    if (imageNumber === 1) {
+        changeImage(id, changeSrc1);
+        imageNumber += 1;
+    } else {
+        changeImage(id, changeSrc2);
+        imageNumber -= 1;
+    }
+}
--- models/change_text.js
+++ models/change_text.js
@@ -2,3 +2,15 @@ function changeText(id, changeText) {
     const x = document.getElementById(id); // 查找元素
     x.innerHTML = changeText; // 修改元素
 }
+
+let textNumber = 1;
+
+function changeText2(id, changeText1, changeText2) {
+    if (textNumber === 1) {
+        changeText(id, changeText1);
+        textNumber += 1;
+    } else {
+        changeText(id, changeText2);
+        textNumber -= 1;
+    }
+}
