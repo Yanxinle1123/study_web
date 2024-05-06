f933cae7cff25912b0bad5fa25aa36212d23be99
yanxinle <1020121123@qq.com>
2024-05-06 21:16:02 +0800

    学习JavaScript: 制作计算器

--- models/calculator.js
+++ models/calculator.js
@@ -1,10 +1,15 @@
 function calculator(text) {
-    return eval(text);
+    return eval(text.replace(/\b0+(\d+)/g, '$1'));
 }
 
 function calculator2(id) {
     try {
-        alert(`计算结果为: ${calculator(getTextBoxValue(id))}`);
+        const result = calculator(getTextBoxValue(id));
+        if (result === undefined) {
+            alert("计算错误");
+        } else {
+            alert(`计算结果为: ${result}`);
+        }
     } catch {
         alert("计算错误");
     }
